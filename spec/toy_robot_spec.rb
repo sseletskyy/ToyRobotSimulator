c724130ad6c31cf5c0ac286aa8f30b2937009c89
Serge Seletskyy <sseletskyy@gmail.com>
2016-06-28 01:51:44 +0300

    add validation for place

--- spec/toy_robot_spec.rb
+++ spec/toy_robot_spec.rb
@@ -7,4 +7,70 @@ RSpec.describe ToyRobot do
         end
     end
 
+    def place
+        @x = @y = 0
+        @direction = 'NORTH'
+    end
+    context 'valid_place_args?' do
+        [
+            {params:['','',''], expected: false},
+            {params:['!','',''], expected: false},
+            {params:['','!',''], expected: false},
+            {params:['5','1','NORTH'], expected: false},
+            {params:['-1','1','NORTH'], expected: false},
+            {params:['1','5','NORTH'], expected: false},
+            {params:['1','-1','NORTH'], expected: false},
+            {params:['1','1','N'], expected: false},
+            {params:['1','1','NORTH'], expected: false},
+            {params:[1,1,'NORTH'], expected: true},
+            {params:[1,1,'EAST'], expected: true},
+            {params:[1,1,'SOUTH'], expected: true},
+            {params:[1,1,'WEST'], expected: true}
+        ]
+        .each do |option|
+            it "check option #{option[:params].to_s} expected #{option[:expected]}" do
+               expect(toy.send('valid_place_args?', *option[:params])).to eq(option[:expected])  
+            end
+        end
+
+    end
+
+    context 'PLACE' do
+        context 'without arguments' do
+            it 'should not change position' do
+                toy.place
+                expect(toy.report).to eq("")
+            end
+        end
+    end
+    context 'with correct arguments' do
+        it 'should change the position' do
+            toy.place(1,1,'NORTH')
+            expect(toy.report).to eq("1,1,NORTH") 
+
+            toy.place(2,2,'SOUTH')    
+            expect(toy.report).to eq("2,2,SOUTH") 
+        end
+    end
+    context 'with incorrect args' do
+        it 'should not change the position' do
+            # case 1 
+            report_str = toy.report
+            toy.place("")
+            expect(toy.report).to eq(report_str)
+
+            # case 2 = incorrect first arg
+            toy.place(2,2,'SOUTH') # set correct    
+            report_str = toy.report
+            toy.place(-5, 4, 'NORTH') # set incorrect
+            expect(toy.report).to eq(report_str)
+
+            # case 3 = incorrect second arg
+            toy.place(2,2,'SOUTH') # set correct    
+            report_str = toy.report
+            toy.place(3, -3, 'SOUTH') # set incorrect
+            expect(toy.report).to eq(report_str)
+
+        end
+    end
 end
--- toy_robot.rb
+++ toy_robot.rb
@@ -1,6 +1,34 @@
 class ToyRobot
+
+    DIRECTIONS = %w(NORTH EAST SOUTH WEST)
+    def initialize
+        @dimension = 5
+        @max_pos = @dimension - 1
+    end
     def report
-        ""
+        return "" unless @direction
+        "#{@x},#{@y},#{@direction}"
     end
 
+    def place(x = nil, y = nil, direction = nil)
+        x = x.to_i
+        y = y.to_i
+        direction = direction.to_s
+        
+        return unless valid_place_args?(x, y, direction)
+        
+        @x = x
+        @y = y
+        @direction = direction
+    end
+
+    private
+    def valid_place_args?(x, y, direction)
+        return false unless x >= 0 && x <= @max_pos
+        return false unless y >= 0 && y <= @max_pos
+        return false unless DIRECTIONS.index(direction)
+        true
+    rescue ArgumentError
+        false 
+    end
 end
